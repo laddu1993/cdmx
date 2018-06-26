c4567f9f525596f1dc09c0abfffbc87d4269234f
laddu1993 <37112438+laddu1993@users.noreply.github.com>
2018-06-26 19:34:18 +0530

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("HOST", "localhost");   // The host you want to connect to.
-define("USER", "root");       // The database username. 
-define("PASSWORD", "");      // The database password. 
-define("DATABASE", "cdmx"); // The database name.
-
-$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
-
-?>
